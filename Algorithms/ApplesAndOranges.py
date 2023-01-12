704220f9be7ce18b88651e26fb6ffb42a7562fad
Monicah Atieno <monicaanestor@gmail.com>
2023-01-12 08:35:40 +0300

    Added the AppleAndOrange function

---
+++ Algorithms/ApplesAndOranges.py
@@ -0,0 +1,66 @@
+#!/bin/python3
+'''
+Sam's house has an apple tree and an orange tree that yield an abundance of fruit. 
+Using the information given below, determine the number of apples and oranges that land on Sam's house.
+In the diagram below:
+
+The red region denotes the house, where  is the start point, and  is the endpoint. The apple tree is to the left of the house, and the orange tree is to its right.
+Assume the trees are located on a single point, where the apple tree is at point , and the orange tree is at point .
+When a fruit falls from its tree, it lands  units of distance from its tree of origin along the -axis. *A negative value of  means the fruit fell  units to the tree's left, and a positive value of  means it falls  units to the tree's right. *
+Apple and orange(2).png
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    # Write your code here
+    apples_on_house = 0
+    oranges_on_house = 0
+    for apple in apples:
+        if (apple + a) >= s and (apple + a) <= t:
+            apples_on_house += 1
+    for orange in oranges:
+        if (orange + b) >= s and (orange + b) <= t:
+            oranges_on_house += 1
+    print(apples_on_house)
+    print(oranges_on_house)
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
