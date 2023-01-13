ed8af70d3652cadf4dacccb161450ff335a4ee9e
Monicah Atieno <monicaanestor@gmail.com>
2023-01-13 17:04:23 +0300

    Added BreakingTheRecord file

---
+++ Algorithms/BreakingTheRecord.py
@@ -0,0 +1,68 @@
+#!/bin/python3
+'''
+Complete the breakingRecords function in the editor below.
+
+breakingRecords has the following parameter(s):
+
+int scores[n]: points scored per game
+Returns
+
+int[2]: An array with the numbers of times she broke her records. Index  is for breaking most points records, and index  is for breaking least points records.
+Input Format
+
+The first line contains an integer , the number of games.
+The second line contains  space-separated integers describing the respective values of .
+
+Constraints
+
+Sample Input 0
+
+9
+10 5 20 20 4 5 2 25 1
+Sample Output 0
+
+2 4
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    # Write your code here
+    min_count = 0
+    max_count = 0
+    min_record = scores[0]
+    max_record = scores[0]
+
+    for score in scores:
+        if score < min_record:
+            min_record = score
+            min_count += 1
+        elif score > max_record:
+            max_record = score
+            max_count += 1
+
+    return [max_count, min_count]
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
